9608366eb43da3e8926e8934512cc33932c05374
Jenny Xu <v-mingx@microsoft.com>
2021-04-01 06:44:17 +0000

    1

---
+++ ConsoleApp1/Program - Copy (4).cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
